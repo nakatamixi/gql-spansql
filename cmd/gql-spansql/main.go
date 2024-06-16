bbc993c9b858728c11fc7a57f4a838d2ace1c721
takeshi.nakata <7553415+nktks@users.noreply.github.com>
2024-06-16 17:36:01 +0900

    update go and support multiple schema path include asterlisk

--- cmd/gql-spansql/main.go
+++ cmd/gql-spansql/main.go
@@ -1,18 +1,38 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/nktks/gql-spansql/internal/converter"
 	"github.com/vektah/gqlparser/v2"
 	"github.com/vektah/gqlparser/v2/ast"
 )
 
+type fschemas []string
+
+func (s *fschemas) String() string {
+	return fmt.Sprint(*s)
+}
+
+func (s *fschemas) Set(value string) error {
+	if len(*s) > 0 {
+		return errors.New("s flag already set")
+	}
+	for _, v := range strings.Split(value, ",") {
+		*s = append(*s, v)
+	}
+	return nil
+}
+
 var (
-	schama      = flag.String("s", "", "path to input schama")
+	schemas     fschemas
 	loose       = flag.Bool("loose", false, "loose type check.")
 	createdName = flag.String("created-column-name", "", "if not empty, add this column as created_at Timestamp column.")
 	updatedName = flag.String("updated-column-name", "", "if not empty, add this column as updated_at Timestamp column.")
@@ -20,18 +40,47 @@ var (
 	columnCase  = flag.String("column-case", "", "snake or lowercamel or uppercamel. if empty no convert.")
 )
 
+func init() {
+	flag.Var(&schemas, "s", "comma-separated path to input schema")
+}
+
 func main() {
 	flag.Parse()
-	body, err := ioutil.ReadFile(*schama)
-	if err != nil {
-		log.Fatalf("Read from file failed: %v", err)
+	var sources []*ast.Source
+	if len(schemas) > 0 {
+		var files []string
+		for _, schema := range schemas {
+			matches, err := filepath.Glob(schema)
+			if err != nil {
+				log.Fatalf("failed to glob schema filename %s: %w", schema, err)
+			}
+			for _, m := range matches {
+				if has(files, m) {
+					continue
+				}
+				files = append(files, m)
+			}
+		}
+		for _, schema := range files {
+			b, err := os.ReadFile(schema)
+			if err != nil {
+				log.Fatalf("Read from file failed: %v", err)
+			}
+			sources = append(sources, &ast.Source{Name: schema, Input: string(b)})
+		}
+	} else {
+		b, err := readStdin()
+		if err != nil {
+			log.Fatalf("Read from stdin failed: %v", err)
+		}
+		sources = append(sources, &ast.Source{Input: string(b)})
 	}
-	schama, err := loadGQL(body)
+	schema, err := loadGQL(sources)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	c, err := converter.NewConverter(schama, *loose, *createdName, *updatedName, *tableCase, *columnCase)
+	c, err := converter.NewConverter(schema, *loose, *createdName, *updatedName, *tableCase, *columnCase)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -43,12 +92,35 @@ func main() {
 
 }
 
-func loadGQL(b []byte) (*ast.Schema, error) {
-	schama, err := gqlparser.LoadSchema(&ast.Source{
-		Input: string(b),
-	})
+func loadGQL(sources []*ast.Source) (*ast.Schema, error) {
+	schema, err := gqlparser.LoadSchema(sources...)
 	if err != nil {
 		return nil, err
 	}
-	return schama, nil
+	return schema, nil
+}
+
+func readStdin() ([]byte, error) {
+	stat, err := os.Stdin.Stat()
+	if err != nil {
+		return []byte{}, err
+	}
+	if (stat.Mode() & os.ModeCharDevice) == 0 {
+		b, err := io.ReadAll(os.Stdin)
+		if err != nil {
+			return []byte{}, err
+		}
+		return b, nil
+	} else {
+		return []byte{}, nil
+	}
+}
+
+func has(ss []string, e string) bool {
+	for _, s := range ss {
+		if s == e {
+			return true
+		}
+	}
+	return false
 }
--- go.mod
+++ go.mod
@@ -1,11 +1,20 @@
 module github.com/nktks/gql-spansql
 
-go 1.16
+go 1.22
 
 require (
 	cloud.google.com/go/spanner v1.19.0
 	github.com/iancoleman/strcase v0.1.3
 	github.com/jinzhu/inflection v1.0.0
-	github.com/stretchr/testify v1.6.1
-	github.com/vektah/gqlparser/v2 v2.2.0
+	github.com/stretchr/testify v1.9.0
+	github.com/vektah/gqlparser/v2 v2.5.16
+)
+
+require (
+	cloud.google.com/go v0.82.0 // indirect
+	github.com/agnivade/levenshtein v1.1.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/exp v0.0.0-20240613232115-7f521ea00fb8 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
